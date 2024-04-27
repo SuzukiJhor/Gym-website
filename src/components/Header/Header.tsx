02adb94046404ad115605412a303f429abc86e04
JhordanS <jhordan.kendy1@gmail.com>
2024-04-27 20:13:00 -0300

    feat: create component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Logo from '../../assets/logo.png'
+import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="header">
+      <img className="logo" src={Logo} alt="Logo"/>
+
+      <ul className="header-menu">
+        <li>Home</li>
+        <li>Programas</li>
+        <li>Sobre Nós</li>
+        <li>Nossos Planos</li>
+        <li>Depoimentos</li>
+      </ul>
+
+    </div>
   )
 }
 
