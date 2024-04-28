ce4e4c4e35155db4728f74cb791d99ad03bd8e45
JhordanS <jhordan.kendy1@gmail.com>
2024-04-28 10:40:22 -0300

    feat: implement map of items from program data

--- src/components/Programs/Programs.tsx
+++ src/components/Programs/Programs.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Programs.css'
+import RightArrow from '../../assets/rightArrow.png'
 import { programsData } from '../../data/programsData';
 
 
@@ -12,7 +13,21 @@ const Programs = () => {
             <span className="stroke-text">Para seu Shape</span>
         </div>
 
-        <div className="program-categories"></div>
+        <div className="program-categories">
+           {programsData.map((program, index)=>{
+                return (
+                <div className="category" key={index}>
+                    {program.image}
+                    <span>{program.heading}</span>
+                    <span>{program.details}</span>
+                    <div className="join-now">
+                        <span>Ver Mais</span>
+                        <img src={RightArrow} alt="" />
+                    </div>
+                </div>
+                )
+            })}
+        </div>
 
     </div>
   )
