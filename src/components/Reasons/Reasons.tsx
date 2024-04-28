36534741a3ee414874756e75caf712c8e01b179b
JhordanS <jhordan.kendy1@gmail.com>
2024-04-28 11:14:11 -0300

    feat: create component reasons

---
+++ src/components/Reasons/Reasons.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Reasons.css'
+import image1 from '../../assets/image1.png'
+import image2 from '../../assets/image2.png'
+import image3 from '../../assets/image3.png'
+import image4 from '../../assets/image4.png'
+import nb from '../../assets/nb.png'
+import adidas from '../../assets/adidas.png'
+import nike from '../../assets/nike.png'
+import tick from '../../assets/tick.png'
+
+function Reasons() {
+  return (
+    <div className="reasons" id="reasons">
+        <div className="left-r">
+            <img src={image1} alt="" />
+            <img src={image2} alt="" />
+            <img src={image3} alt="" />
+            <img src={image4} alt="" />
+        </div>
+        <div className="right-r"> 
+            <span>Algumas Razões</span>
+
+            <div>
+                <span className="stroke-text">Porque </span>
+                <span>Continuam Nos Escolhendo ?</span>
+            </div>
+
+            <div className="details-r">
+                <div>
+                    <img src={tick} alt="" />
+                    <span>+140 Treinadores Especializados </span>
+                </div>
+                <div>
+                    <img src={tick} alt="" />
+                    <span>Performance Mais Inteligente e Mais Rápida</span>
+                </div>
+                <div>
+                    <img src={tick} alt="" />
+                    <span>Programas FREE para Novos Membros</span>
+                </div>
+                <div>
+                    <img src={tick} alt="" />
+                    <span>Parceiros confiáveis</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Reasons
